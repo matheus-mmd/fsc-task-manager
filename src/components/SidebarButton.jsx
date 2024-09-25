d5e73e270e27d61d11adf36b63d7f78ba210745a
matheus-mmd <matheus_meneses2011@hotmail.com>
2024-09-24 21:39:14 -0300

    chore: add prop-types to SidebarButton component

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,22 +1,27 @@
-const SidebarButton = ({ children, variant }) => {
-  const getVariantClasses = () => {
-    if (variant === "unselected") {
-      return "text-brand-dark-blue"
-    }
+import PropTypes from "prop-types"
+import { tv } from "tailwind-variants"
 
-    if (variant === "selected") {
-      return "bg-brand-primary bg-opacity-15 text-brand-primary"
-    }
-  }
+const SidebarButton = ({ children, color }) => {
+  const sidebar = tv({
+    base: "flex items-center gap-2 rounded-lg px-6 py-3",
+    variants: {
+      color: {
+        selected: "bg-brand-primary bg-opacity-15 text-brand-primary",
+        unselected: "text-brand-dark-blue",
+      },
+    },
+  })
 
   return (
-    <a
-      href="#"
-      className={`flex items-center gap-2 rounded-lg px-6 py-3 ${getVariantClasses()}`}
-    >
+    <a href="#" className={sidebar({ color })}>
       {children}
     </a>
   )
+}
+
+SidebarButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(["selected", "unselected"]),
 }
 
 export default SidebarButton
